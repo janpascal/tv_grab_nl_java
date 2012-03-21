9c942116c02ff447bcf032b2a3b0aac5164b918b
Jan-Pascal van Best <janpascal@vanbest.org>
2012-03-21 14:18:33 +0100

    Release 0.5, created release.sh script

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VERSION=0.5.0
+
+tmpdir=$( mktemp -d ) || exit 1
+DESTDIR="$tmpdir/tv_grab_nl_java-$VERSION"
+ZIPFILE="$PWD/../tv_grab_nl_java-$VERSION.zip"
+
+mkdir "$DESTDIR"
+rm -f "$ZIPFILE"
+
+mvn assembly:single
+cp target/tv_grab_nl_java-$VERSION-dep.jar "$DESTDIR/tv_grab_nl_java.jar"
+cp tv_grab_nl_java README Changelog "$DESTDIR"
+
+pushd "$tmpdir"
+zip -r "$ZIPFILE" *
+popd
+
+rm -rf $tmpdir
