c7b7850e53eeae77b7a7b8869e1f35f36ca2b792
Jan-Pascal van Best <janpascal@vanbest.org>
2012-03-23 17:00:22 +0100

    GPL license toegevoegd in README en alle source files

--- src/main/java/org/vanbest/xmltv/Channel.java
+++ src/main/java/org/vanbest/xmltv/Channel.java
@@ -1,5 +1,21 @@
 package org.vanbest.xmltv;
 
+/*
+  Copyright (c) 2012 Jan-Pascal van Best <janpascal@vanbest.org>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  The full license text can be found in the LICENSE file.
+*/
+
 public class Channel {
 	int id;
     String name;
