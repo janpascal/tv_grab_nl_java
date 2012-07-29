1b3c9aee17b738e685735509220ab2d79878d4a7
Jan-Pascal van Best <janpascal@vanbest.org>
2012-07-29 23:09:56 +0200

    Add initial test for Main class

---
+++ src/test/java/org/vanbest/xmltv/MainTest.java
@@ -0,0 +1,47 @@
+package org.vanbest.xmltv;
+
+import static org.junit.Assert.*;
+
+import java.io.OutputStream;
+import java.io.StringWriter;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.SimpleLayout;
+import org.apache.log4j.WriterAppender;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void testShowHeader() {
+		Logger logger = Logger.getLogger(Main.class);
+		StringWriter  writer = new StringWriter();
+		logger.addAppender(new WriterAppender(new SimpleLayout(), writer));
+		Main main = new Main();
+		main.showHeader();
+		String result = writer.toString();
+		assertTrue(result.contains("ABSOLUTELY NO WARRANTY"));
+		// fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConfigure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShowLicense() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDefaultConfigFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
